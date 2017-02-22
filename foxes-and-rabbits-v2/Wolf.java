8614cf341ce0ec965e5b3fb10d91455b2113f44c
FRANKENSTEIN\ThomasSTodal <thomatod@stud.ntnu.no>
2017-02-22 20:40:57 +0100

    asdf

--- foxes-and-rabbits-v2/Wolf.java
+++ foxes-and-rabbits-v2/Wolf.java
@@ -2,12 +2,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author ThomasSTodal, VinhKTran, KristofferMartinsen
